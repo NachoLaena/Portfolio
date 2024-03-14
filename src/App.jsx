6496f0cfc96970e1bd682516ca8b17bc17b59795
NachoLaena <ignalaribe@gmail.com>
2024-03-14 01:21:11 -0300

    Add aboutMe component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/hero";
 import Projects from "./components/projects";
+import AboutMe from "./components/aboutMe";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="bg-[#000000] bg-fixed bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
         <div className="max-w-5xl mx-auto p-4">
           <Hero />
+          <AboutMe />
           <Projects />
         </div>
       </div>
