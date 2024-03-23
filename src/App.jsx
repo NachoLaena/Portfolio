5de4480d09c3d69a5996dd2d0d5736e1d9bb9d29
NachoLaena <ignalaribe@gmail.com>
2024-03-23 17:23:42 -0300

    Change background style and remove aboutMe component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Hero from "./components/hero";
 import Projects from "./components/projects";
-import AboutMe from "./components/aboutMe";
 
 function App() {
   return (
     <main>
-      <div className="bg-[#000000] bg-fixed bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-        <div className="max-w-5xl mx-auto p-4">
+      <div className=" bg-[#00091d] bg-fixed bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:40px_40px] p-4">
+        <div className="max-w-5xl mx-auto flex flex-col gap-20 md:py-20">
           <Hero />
-          <AboutMe />
           <Projects />
         </div>
       </div>
